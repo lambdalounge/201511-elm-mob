fa6a6548fdb703d8cebde8ceba6db83ee78226dc
Jessica Kerr <jessitron@outpace.com>
2015-09-21 15:51:52 +0000

    Initial commit (of Mob FizzBuzz, yo)

---
+++ FizzBuzz.elm
@@ -0,0 +1,12 @@
+module FizzBuzz where
+
+import Html exposing (Html)
+import Html.Attributes as Attributes
+import Html.Events as Events
+
+-- talk in questions
+-- facilitator at the back of the room, off to the side, unless there's a purpose
+-- work together first. before good code.
+
+main : Html
+main = Html.text "Hello world"
