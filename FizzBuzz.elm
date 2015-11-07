504dbf16bda409a149e32976eef35dbd3e7e113f
Jessica Kerr <jessitron@outpace.com>
2015-11-07 16:23:43 +0000

    final

--- FizzBuzz.elm
+++ FizzBuzz.elm
@@ -4,10 +4,22 @@ import Html exposing (Html)
 import Html.Attributes as Attributes
 import Html.Events as Events
 
+fizz : Int -> String
+fizz x = if (0 == x % 3) then "Fizz" else ""
+
+buzz : Int -> String
+buzz x = if (0 == x % 5) then "Buzz" else ""
+
+fizzbuzz : Int -> String
+fizzbuzz x = fizz x ++ buzz x
+
+fizzbuzzornum : Int -> String
+fizzbuzzornum x = let maybe_strings = fizzbuzz x in
+  if maybe_strings == "" then toString x else maybe_strings 
+
 main : Html
 main = 
   Html.pre [Attributes.style [("margin", "10px")]]
-   [ 
-     Html.text " Hello Lambda Lounge" 
-   ]
+     (List.map (Html.text << (((++)"\n")) << fizzbuzzornum) [1..100])
+   
  
